43ce660796670c4f229f8869024821de671b8dbb
TaoHong <1940713370@qq.com>
2019-10-31 18:24:37 +0000

    buaa

---
+++ app/src/main/java/oak/shef/ac/uk/myapplication/ImageElement.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019. This code has been developed by Fabio Ciravegna, The University of Sheffield. All rights reserved. No part of this code can be used without the explicit written permission by the author
+ */
+
+package oak.shef.ac.uk.myapplication;
+
+public class ImageElement {
+    int image=-1;
+
+    public ImageElement(int image)
+    {
+        this.image = image;
+    }
+}
